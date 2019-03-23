c0f1c3f6b15c520a064153321ff2997a687c210d
David Baynard <davidbaynard@gmail.com>
2019-03-23 04:40:31 +0000

    Track breadcrumbs during section traversal

--- mtags/src/MTags/Parser.hs
+++ mtags/src/MTags/Parser.hs
@@ -13,12 +13,14 @@
 {-# LANGUAGE DerivingVia                #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE NamedFieldPuns             #-}
 {-# LANGUAGE NoImplicitPrelude          #-}
 {-# LANGUAGE OverloadedLists            #-}
+{-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE PackageImports             #-}
 {-# LANGUAGE PatternSynonyms            #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE ViewPatterns               #-}
-{-# LANGUAGE NamedFieldPuns #-}
 
 module MTags.Parser
   ( Commonmark
@@ -35,7 +37,9 @@ import           "base" Data.Coerce                        (coerce)
 import           "prettyprinter" Data.Text.Prettyprint.Doc
 import           "validity" Data.Validity
 import           "rio" RIO
-import           "rio" RIO.Seq                             (Seq)
+import           "rio" RIO.Seq                             (Seq ((:|>)))
+import qualified "rio" RIO.Seq                             as Seq (take)
+import qualified "rio" RIO.Text                            as T (intercalate)
 
 tagsFromCmark :: HeadingTag tag -> Commonmark -> Seq tag
 tagsFromCmark f = tagsFromNode f . commonmarkToNode [] . coerce
@@ -52,11 +56,18 @@ readCommonmark = fmap Commonmark . readFileUtf8
 
 type HeadingTag tag = HeadingLevel -> LineNo -> Text -> tag
 
-tagsFromNode :: HeadingTag tag -> Node -> Seq tag
-tagsFromNode f = fix $ \go -> \case
-  (Document ns)   -> foldMap go ns
-  (Heading n p t) -> [f n p t]
-  _               -> []
+tagsFromNode :: forall tag . HeadingTag tag -> Node -> Seq tag
+tagsFromNode f = fst . go []
+  where
+    go :: Seq Text -> Node -> (Seq tag, Seq Text)
+    go stack = \case
+      -- \go stack -> \case
+      (Document ns)   -> foldM go [] ns
+      -- This case shouldn't happen
+      (Heading 0 p t) -> ([f 1 p t], [t])
+      (Heading n p t) -> let nstack = Seq.take (fromIntegral n - 1) stack :|> t in
+        ([f n p . T.intercalate "|" . toList $ nstack], nstack)
+      _               -> ([], stack)
 
 pattern Document :: [Node] -> Node
 pattern Document ns <- Node _ DOCUMENT ns
