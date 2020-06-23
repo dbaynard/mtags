dbaynard/mtags

commit 3f0ef64f56a522b373fe199f01ba82ad621340d1

Rearrange MTabs module

mtags/src/MTags.hs
@@ -83,10 +83,6 @@ import           "rio" RIO
 import qualified "rio" RIO.List                            as L (any)
 import qualified "rio" RIO.Text                            as T (any)
 
---------------------------------------------------
--- * Individual tags
---------------------------------------------------
-
 -- $setup
 --
 -- >>> :set -XOverloadedStrings
@@ -102,6 +98,10 @@ import qualified "rio" RIO.Text                            as T (any)
 --       }
 -- :}
 
+--------------------------------------------------
+-- * Individual tags
+--------------------------------------------------
+
 -- | A single line, corresponding to a single tag.
 data MTag = MTag
   { tagName    :: TagName    -- ^ Identifier
@@ -113,7 +113,7 @@ data MTag = MTag
   deriving anyclass (Validity)
 
 -- | >>> renderPretty exampleMTag
--- Analysis of proteins	cry.md	/^## Analysis of proteins$/;"	s	line:197	section:Methods
+-- Analysis of proteins cry.md  /^## Analysis of proteins$/;" s line:197  section:Methods
 instance Pretty MTag where
   pretty m = mconcat
     [ m ^. typed @TagName . to pretty
@@ -124,6 +124,29 @@ instance Pretty MTag where
     , ";\""
     , m ^. typed @TagFields . to pretty
     ]
+
+--------------------------------------------------
+-- ** Possible field values
+--------------------------------------------------
+
+data FieldValue
+  = Kind TagKind
+  | Line LineNo
+  | Section SectionName
+  deriving stock (Show, Eq, Ord, Generic)
+  deriving anyclass (Validity)
+
+instance Pretty FieldValue where
+  pretty (Kind t) = pretty t
+
+  pretty (Line n)    = mconcat ["line", colon, pretty n]
+  pretty (Section n) = mconcat ["section", colon, pretty n]
+
+  prettyList = foldMap $ (tab <>) . pretty
+
+--------------------------------------------------
+-- ** Newtypes
+--------------------------------------------------
 
 newtype TagName = TagName Text
   deriving stock (Generic)
@@ -148,21 +171,6 @@ newtype TagFields = TagFields (Set FieldValue)
 instance Pretty TagFields where
   pretty (TagFields fieldSet) = prettyList . toList $ fieldSet
 
-data FieldValue
-  = Kind TagKind
-  | Line LineNo
-  | Section SectionName
-  deriving stock (Show, Eq, Ord, Generic)
-  deriving anyclass (Validity)
-
-instance Pretty FieldValue where
-  pretty (Kind t) = pretty t
-
-  pretty (Line n)    = mconcat ["line", colon, pretty n]
-  pretty (Section n) = mconcat ["section", colon, pretty n]
-
-  prettyList = foldMap $ (tab <>) . pretty
-
 newtype TagKind = TagKind Text
   deriving stock (Generic)
   deriving newtype (Show, Eq, Ord, IsString, Pretty)
@@ -179,14 +187,18 @@ newtype SectionName = SectionName Text
   deriving Validity via (NoChar "\t" Text)
 
 --------------------------------------------------
--- * PrettyPrinting
+-- * Helpers
+--------------------------------------------------
+
+--------------------------------------------------
+-- ** PrettyPrinting
 --------------------------------------------------
 
 tab :: Doc ann
 tab = "\t"
 
 --------------------------------------------------
--- * Validity
+-- ** Validity
 --------------------------------------------------
 
 charNotIn :: Char -> Text -> Bool

commit 36bfd163426a6f69f2811beec7c08e53b4c1ec98

Fix tabs/spaces issue
Introduced in 3f0ef64f56a522b373fe199f01ba82ad621340d1

mtags/src/MTags.hs
@@ -152,7 +152,7 @@ instance Ord MTag where
   compare = comparing tagName
 
 -- | >>> renderPretty exampleMTag
--- Analysis of proteins cry.md  /^## Analysis of proteins$/;" s line:197  section:Methods
+-- Analysis of proteins	cry.md	/^## Analysis of proteins$/;"	s	line:197	section:Methods
 instance Pretty MTag where
   pretty m = mconcat
     [ m ^. typed @TagName . to pretty