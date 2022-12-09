8d5f768ee4831429bced3c3bbe0a54260ddc432e
David Baynard <git@baynard.dev>
2022-12-09 21:33:12 +0000

    Introduce flake.nix to build derivation

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Markdown tags, i.e. mtags";
+
+  inputs = {
+    dbaynard.url = "github:dbaynard/flakes";
+    dbaynard.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, dbaynard, self, ... }:
+    let
+      inherit (dbaynard) lib;
+
+    in
+    {
+      overlays.default = final: prev: {
+        default = prev.haskellPackages.callPackage ./. { };
+      };
+    }
+    // lib.foldFor [ "aarch64-darwin" "x86_64-linux" "aarch64-linux" ]
+      (system: {
+        packages.${system} = self.overlays.default
+          self.packages.${system}
+          nixpkgs.legacyPackages.${system};
+      });
+}
