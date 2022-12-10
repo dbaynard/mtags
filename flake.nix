7117473fbc7bc9563d4ccd19d872532c298b103c
David Baynard <git@baynard.dev>
2022-12-10 00:59:59 +0000

    Fix package name in overlay

--- flake.nix
+++ flake.nix
@@ -13,18 +13,19 @@
     in
     {
       overlays.default = final: prev: {
-        default = prev.haskellPackages.callPackage ./. { };
+        mtags = prev.haskellPackages.callPackage ./. { };
       };
     }
-    // lib.foldFor [ "aarch64-darwin" "x86_64-linux" "aarch64-linux" ]
-      (system: {
-        packages.${system} = self.overlays.default
+    // lib.foldFor [ "aarch64-darwin" "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        packages = self.overlays.default
           self.packages.${system}
           nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.${system} = packages // { default = packages.mtags; };
 
         devShells.${system}.default =
-          nixpkgs.legacyPackages.${system}.callPackage ./shell.nix {
-            inherit (self.packages.${system}) default;
-          };
+          nixpkgs.legacyPackages.${system}.callPackage ./shell.nix { };
       });
 }
--- shell.nix
+++ shell.nix
@@ -1,5 +1,4 @@
 { lib
-, default
 , haskellPackages
 , mkShell
 , zsh
